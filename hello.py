4d4c2234ed6e568ddbbd8f7b7264e40e11039e08
GARV2026 <trgarv@gmv.com>
2026-06-29 09:33:41 +0200

    monday branch

--- hello.py
+++ hello.py
@@ -5,3 +5,6 @@ print("Making changes in the branch")
 print("Goodbye from farewell branch")
 print("Hello from the second branch")
 
+print("I am going to make a simple code so that i can learn how to merge properly")
+for i in range(5):
+    print("simple loop iteration", i)
