ffff91530770e1d9ad545031c0d4bb12a3f15a99
GARV2026 <trgarv@gmv.com>
2026-06-29 09:42:49 +0200

    matrix

--- hello.py
+++ hello.py
@@ -8,3 +8,14 @@ print("Hello from the second branch")
 print("SIMPLE MATHS")
 print(5 - 3)
 print(4 * 6)
+
+print("MATRIX MULTIPLICATION")
+import numpy as np
+A = np.array([[1, 2, 3],
+    [0, 1, 0],
+    [1, 1, 1]])
+
+B = np.array([[3], [3], [0]])
+
+
+print(A @B)
